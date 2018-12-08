19cf9f6523078c61e23e36b5ebf786c1f91b2f2a
appgenny <appgenerator2@gmail.com>
2018-12-09 01:22:18 +0500

    adasdasd

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.bunkerpalace.cordova.YoutubeVideoPlayer/plugins/com.bunkerpalace.cordova.YoutubeVideoPlayer/www/YoutubeVideoPlayer.js",
+        "id": "com.bunkerpalace.cordova.YoutubeVideoPlayer.YoutubeVideoPlayer",
+        "pluginId": "com.bunkerpalace.cordova.YoutubeVideoPlayer",
+        "clobbers": [
+            "YoutubeVideoPlayer"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.bunkerpalace.cordova.YoutubeVideoPlayer": "1.0.5",
+    "cordova-admob-sdk": "0.20.0"
+}
+// BOTTOM OF METADATA
+});
